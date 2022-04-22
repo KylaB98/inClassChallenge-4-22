f17cf4d86986843eb292f23b951fbb6c6fce8a39
Kyla Wootson <kylawootson@MacBook-Pro.local>
2022-04-22 13:48:02 -0400

    Added Challenge file

---
+++ fridayChallenge.js
@@ -0,0 +1,16 @@
+//1) Create 3 arrays, One with all String data types, one with all Numbers data types, and one with Boolean data types.  You will need to add sample data to each array (don't over think this). Each array should have at least 5 elements.. 
+
+let firstNames = ['Stephen', 'Caroline', 'Damon', 'Elena', 'Bonnie', 'Enzo'];
+let ages = [145, 22,  150, 23, 23 , 140];
+let isVampire = [true, true, true, false, false, true];
+
+//2) Next create a function that can take any of these arrays as a parameter and add an additional String, Number, or Boolean depending on which array was passed in. 
+
+console.log(firstNames.push('Katherine'));
+
+if (firstNames[6]= 'Katherine'){
+    console.log('Katherine has been added')
+}
+else{
+    console.log('Katherine has not been added')
+};
